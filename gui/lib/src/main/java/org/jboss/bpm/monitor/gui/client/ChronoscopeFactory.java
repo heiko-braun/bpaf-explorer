06326651ee05d35486c10daf7ae67d39df492015
heiko-braun <hbraun@redhat.com>
2010-11-01 15:58:35 +0100

    set version 1.0.0-Beta1

---
+++ gui/lib/src/main/java/org/jboss/bpm/monitor/gui/client/ChronoscopeFactory.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2009 JBoss, a divison Red Hat, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.bpm.monitor.gui.client;
+
+import org.timepedia.chronoscope.client.ChronoscopeOptions;
+import org.timepedia.chronoscope.client.browser.Chronoscope;
+
+/**
+ * @author: Heiko Braun <hbraun@redhat.com>
+ * @date: Oct 27, 2010
+ */
+public class ChronoscopeFactory {
+    public static Chronoscope chronoscope = null;
+
+    public static Chronoscope getInstance()
+    {
+        if(null==chronoscope)
+        {
+            // Chronoscope.enableHistorySupport(true);
+            Chronoscope.setFontBookRendering(true);
+            ChronoscopeOptions.setErrorReporting(true);
+            Chronoscope.setMicroformatsEnabled(false);
+            Chronoscope.setShowCredits(false);
+            Chronoscope.initialize();
+
+
+            chronoscope = Chronoscope.getInstance();
+        }
+
+        return chronoscope;
+    };
+}
